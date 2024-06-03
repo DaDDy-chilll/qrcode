cc6b73d8d88b4a839127bc075cfc199a757453f9
DaDDy <blacklistuser420@gmail.com>
2024-06-03 12:53:05 +0630

    update

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const qr = require('qrcode')
 const express = require('express');
 const app = express();
-
+const path = require('path')
 let data = {
     id:1,
     name:'user1',
     email:'user1@gmail.com'
 };
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname,'public')));
 
 let strData = JSON.stringify(data)
 
@@ -20,7 +24,10 @@ const qrData = qr.toDataURL(strData,function(err,code){
     if(err) return console.log('Error occured in Data URL generation');
     else return code;
 })
+
+
 app.get('/',(req,res) => {
-    res.send(qrCode);
+    res.render('index');
+    // res.send(qrCode);
 })
 app.listen(3000,() => console.log('server is running on port 3000'))
